464c6288717816999c49f898ee55d6051c16dfc6
Nick <nick@m-is.net>
2024-08-07 20:05:48 -0600

    added sql file for creating the database

---
+++ dbCreate.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Fighters (
+    fighterId int PRIMARY KEY AUTO_INCREMENT,
+    fighterName varchar(120) UNIQUE,
+    strikes int,
+    INDEX idx_fighterData (fighterId,fighterName,strikes)
+);
+
+CREATE TABLE Matches (
+    matchId int PRIMARY KEY AUTO_INCREMENT,
+    matchRing int
+);
+
+CREATE TABLE Bouts (
+    boutId int PRIMARY KEY AUTO_INCREMENT,
+    fighterColor varchar(20),
+    fighterId int,
+    matchId int,
+    UNIQUE (fighterId, matchId),
+    FOREIGN KEY (fighterId) REFERENCES Fighters(fighterId),
+    FOREIGN KEY (matchId) REFERENCES Matches(matchId),
+    INDEX idx_fighter_match (fighterId, matchId)
+);
+
+CREATE TABLE Bout_Score(
+    scoreId int PRIMARY KEY AUTO_INCREMENT,
+    contact bit,
+    target bit,
+    control bit,
+    FOREIGN KEY (boutId) REFERENCES Bouts(boutId)
+);
+
+
