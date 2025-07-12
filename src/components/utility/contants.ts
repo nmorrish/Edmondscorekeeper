b9fe90f11647ef1667ec6e00146e588bae42e525
Nick <nick@m-is.net>
2025-07-12 02:52:36 -0600

    added latest stable code

--- src/components/utility/contants.ts
+++ src/components/utility/contants.ts
@@ -15,5 +15,5 @@
 /* the domain path containing the server side execution scripts is declared here. Comment/uncomment whichever host is being used. */
 // export const domain_uri = "https://ec-reciever.m-is.net";
 // export const domain_uri = "https://ec2-receiver.m-is.net";
-export const domain_uri = "http://localhost/Edmondscorekeeper/phpFiles";
-// export const domain_uri = "http://68.149.96.12:25565/ec-receiver";
+// export const domain_uri = "http://localhost/Edmondscorekeeper/phpFiles";
+export const domain_uri = "http://68.149.96.12:25565/ec-receiver";
